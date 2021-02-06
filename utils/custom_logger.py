82fda228cd37713fa41ac3b28ee88d2615264490
Leonardo Cencetti <leonardo.cencetti@epfl.ch>
2021-02-06 11:49:47 +0100

    [dev] Start new branch

---
+++ utils/custom_logger.py
@@ -0,0 +1,7 @@
+#
+# File created by Leonardo Cencetti on 2/5/2021
+#
+import logging
+
+FORMAT = '[%(levelname)s][%(name)s] %(message)s \t (%(filename)s:%(lineno)d)'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
